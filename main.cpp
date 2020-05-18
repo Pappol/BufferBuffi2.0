358a7003225503136e55b70b86e1d2cd12be9dc5
riccardo <riccardo.peron@studenti.unitn.it>
2020-05-18 20:20:37 +0200

    :memo: Prima implementazione ricerca rettangoli
    Co-authored-by: Riccardo Parola <47860088+Pappol@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 //git testing
+
+int punti = 0;
 // Pair: row and column
 typedef pair<ushort, ushort> rc;
 
@@ -18,8 +20,12 @@ typedef struct {
 // Checks for an optimal rectangular solution. If it is found it is printed and
 // the program exits w/ code 0. If a solution is found but it's not optimal it
 // gets printed anyway.
-bool checkForRectangles(int n, int m, vector<rc> w, vector<rc> b,
-                        vector<vector<cell>> matrix);
+string checkForRectangles(int n, int m, vector<rc> w, vector<rc> b, vector<vector<cell>> matrix);
+string RecTl(rc TL, int n, int m, vector<rc> w, vector<rc> b, vector<vector<cell>> matrix);
+int points(int A, int B, int W, rc start, rc end);
+
+
+bool equals(rc a, rc b);
 
 int main(int argc, char **argv) {
   ushort N, M;
@@ -67,4 +73,140 @@ int main(int argc, char **argv) {
     cout << endl;
   }
   return 0;
+}
+
+bool equals(rc a, rc b){
+  if(a.first == b.first && a.second == b.second) return true;
+  return false;
+}
+
+string checkForRectangles(int n, int m, vector<rc> w, vector<rc> b, vector<vector<cell>> matrix){
+  rc TL;
+  int i, j;
+  string ret;
+
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+
+      if(matrix[i][j].type == 2){
+        TL.first = i;
+        TL.second = j;
+        string tmp=RecTl(TL,n,m,w,b,matrix);
+          if(tmp!=" "){
+            return tmp;            
+          }
+      } else if (matrix[i][j].type == 2){
+        //codice x bianco
+        if(i>0){
+          TL.first=i-1;
+          TL.second=j;
+          string tmp=RecTl(TL,n,m,w,b,matrix);
+          if(tmp!=" "){
+            return tmp;            
+          }
+        }
+        if(j>0){
+          TL.first=i;
+          TL.second=j-1;
+          string tmp=RecTl(TL,n,m,w,b,matrix);
+          if(tmp!=" "){
+            return tmp;            
+          }
+        }
+      }
+    }
+  }
+
+}
+
+string RecTl(rc TL, int n, int m, vector<rc> w, vector<rc> b, vector<vector<cell>> matrix){
+  stringbuf percorso;
+  string ret = " ";
+  int n_anelli = 0, i, j;
+  rc start;
+
+  n_anelli++;
+  start.first = TL.first;
+  start.second = TL.second;
+  
+  i = TL.first;
+  for(int j=TL.second; j<n; j++){
+    percorso.sputc('R');
+    if(matrix[i][j].type == 2 || matrix[i+1][j].type == 1){
+      n_anelli++;
+      TL.first = i;
+      TL.second = j;
+      break;
+    } else if (matrix[i][j].type == 1){
+        TL.first = i;
+        TL.second = j+1;
+        n_anelli++;
+        break;
+    }
+  }
+
+  j = TL.second;
+  for(int i=TL.first; i<m; i++){
+    percorso.sputc('D');
+    if(matrix[i][j].type == 2 || matrix[i][j-1].type == 1){
+      n_anelli++;
+      TL.first = i;
+      TL.second = j;
+      break;
+    } else if (matrix[i][j].type == 1){
+        n_anelli++;
+        TL.first = i+1;
+        TL.second = j;
+        break;
+    }
+  }
+
+  i = TL.first;
+  for(int j=TL.second; j>=0; j--){
+    percorso.sputc('L');
+    if(matrix[i][j].type == 2 || matrix[i-1][j].type == 1){
+      n_anelli++;
+      TL.first = i;
+      TL.second = j;
+      break;
+    } else if (matrix[i][j].type == 1){
+        n_anelli++;
+        TL.first = i;
+        TL.second = j-1;
+        break;
+    }
+  }
+
+  j = TL.second;
+  for(int i=TL.first; i>=0; i--){
+    percorso.sputc('U');
+    if(matrix[i][j].type == 2 || matrix[i][j+1].type == 1){
+      n_anelli++;
+      TL.first = i;
+      TL.second = j;
+      break;
+    } else if (matrix[i][j].type == 1){
+        n_anelli++;
+        TL.first = i-1;
+        TL.second = j;
+        break;
+    }
+  }
+
+  if(!equals(start, TL)) return ret;
+  else {
+    ret = percorso.str();
+    return ret;
+  }
+
+}
+
+int points(int A, int B, int W, rc start, rc end){
+  int tmp_point;
+  if(start.first==end.first && start.second==end.second){
+    tmp_point = 5*(A/(B+W));
+  }else{
+    tmp_point = 5*(A/(B+W))*0.5;
+  }
+  punti = max(punti, tmp_point);
 }
