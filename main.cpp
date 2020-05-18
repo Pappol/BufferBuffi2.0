c7cac9434f63e2e49bb3c539b66e4ccab6e83a5d
Giacomo Zanolli <giacozanolli+gitlab@gmail.com>
2020-05-18 15:02:39 +0200

    Add comments, change short in ushort

--- main.cpp
+++ main.cpp
@@ -3,25 +3,29 @@
 
 using namespace std;
 
+// Pair: row and column
+typedef pair<ushort, ushort> rc;
+
 int main(int argc, char **argv) {
-  short N, M;
+  ushort N, M;
   int B, W;
   ifstream in("input.txt");
   ofstream out("output.txt");
+  // Initialization
   in >> N >> M >> B >> W;
-  vector<pair<short, short>> blacks = vector<pair<short, short>>(B);
+  vector<rc> blacks = vector<rc>(B);
   for (int i = 0; i < B; i++) {
-    short r, c;
+    ushort r, c;
     in >> r >> c;
-    blacks[i] = pair<short, short>(r, c);
+    blacks[i] = rc(r, c);
   }
-  vector<pair<short, short>> whites = vector<pair<short, short>>(W);
+  vector<rc> whites = vector<rc>(W);
   for (int i = 0; i < W; i++) {
-    short r, c;
+    ushort r, c;
     in >> r >> c;
-    whites[i] = pair<short, short>(r, c);
+    whites[i] = rc(r, c);
   }
-
+  
   cout << "hello word!\n\n";
   out << (N + M) << endl;
   return 0;
