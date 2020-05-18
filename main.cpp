4bc7aee421bc044c3969ab8e0f576317c00d30e7
Giacomo Zanolli <giacozanolli+gitlab@gmail.com>
2020-05-18 18:42:45 +0200

    Add cell structure

--- main.cpp
+++ main.cpp
@@ -6,27 +6,65 @@ using namespace std;
 // Pair: row and column
 typedef pair<ushort, ushort> rc;
 
+// Cell data structure
+typedef struct {
+  bool U = true;  // Can go up from here
+  bool D = true;  // Can go down from here
+  bool L = true;  // Can go left from here
+  bool R = true;  // Can go right from here
+  short type = 0; // 0=vuota,1=bianca, 2=nera
+} cell;
+
+// Checks for an optimal rectangular solution. If it is found it is printed and
+// the program exits w/ code 0. If a solution is found but it's not optimal it
+// gets printed anyway.
+bool checkForRectangles(int n, int m, vector<rc> w, vector<rc> b,
+                        vector<vector<cell>> matrix);
+
 int main(int argc, char **argv) {
   ushort N, M;
   int B, W;
   ifstream in("input.txt");
-  ofstream out("output.txt");
   // Initialization
   in >> N >> M >> B >> W;
+  vector<vector<cell>> matrix(M, vector<cell>(N, cell()));
+  // Forbidding invalid moves
+  for (int i = 0; i < N; i++) {
+    matrix[0][i].U = false;
+  }
+  for (int i = 0; i < M; i++) {
+    matrix[i][0].L = false;
+    matrix[i][N - 1].R = false;
+  }
+  for (int i = 0; i < N; i++) {
+    matrix[M - 1][i].D = false;
+  }
+  // Getting rings' poitions
   vector<rc> blacks = vector<rc>(B);
   for (int i = 0; i < B; i++) {
     ushort r, c;
     in >> r >> c;
     blacks[i] = rc(r, c);
+    matrix[r][c].type = 2;
   }
   vector<rc> whites = vector<rc>(W);
   for (int i = 0; i < W; i++) {
     ushort r, c;
     in >> r >> c;
     whites[i] = rc(r, c);
+    matrix[r][c].type = 1;
   }
-  
-  cout << "hello word!\n\n";
-  out << (N + M) << endl;
+  // Checking if a rectangle could be the optimal solution
+  if (whites.size() <= 8 && blacks.size() <= 4) {
+    checkForRectangles(N, M, whites, blacks, matrix);
+  }
+  ofstream out("output.txt");
+
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << matrix[i][j].type << " ";
+    }
+    cout << endl;
+  }
   return 0;
 }
