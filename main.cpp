2cf4e86786facbbd4b4c7eb465c9a33b8f385ec9
Pappol <riccardo.parola@studenti.unitn.it>
2020-05-19 11:57:07 +0200

    init

--- main.cpp
+++ main.cpp
@@ -2,69 +2,121 @@
 #include <fstream>
 
 using namespace std;
-//git testing
+
 // Pair: row and column
 typedef pair<ushort, ushort> rc;
 
 // Cell data structure
-typedef struct {
-  bool U = true;  // Can go up from here
-  bool D = true;  // Can go down from here
-  bool L = true;  // Can go left from here
-  bool R = true;  // Can go right from here
-  short type = 0; // 0=vuota,1=bianca, 2=nera
+typedef struct{
+	bool U = true;	// Can go up from here
+	bool D = true;	// Can go down from here
+	bool L = true;	// Can go left from here
+	bool R = true;	// Can go right from here
+	short type = 0; // 0=vuota,1=bianca, 2=nera, 3=gia passata
 } cell;
 
-// Checks for an optimal rectangular solution. If it is found it is printed and
-// the program exits w/ code 0. If a solution is found but it's not optimal it
-// gets printed anyway.
-bool checkForRectangles(int n, int m, vector<rc> w, vector<rc> b,
-                        vector<vector<cell>> matrix);
+int find(rc start,rc position,vector<vector<cell>> matrix,stringbuf *giro);
 
-int main(int argc, char **argv) {
-  ushort N, M;
-  int B, W;
-  ifstream in("input.txt");
-  // Initialization
-  in >> N >> M >> B >> W;
-  vector<vector<cell>> matrix(M, vector<cell>(N, cell()));
-  // Forbidding invalid moves
-  for (int i = 0; i < N; i++) {
-    matrix[0][i].U = false;
-  }
-  for (int i = 0; i < M; i++) {
-    matrix[i][0].L = false;
-    matrix[i][N - 1].R = false;
-  }
-  for (int i = 0; i < N; i++) {
-    matrix[M - 1][i].D = false;
-  }
-  // Getting rings' poitions
-  vector<rc> blacks = vector<rc>(B);
-  for (int i = 0; i < B; i++) {
-    ushort r, c;
-    in >> r >> c;
-    blacks[i] = rc(r, c);
-    matrix[r][c].type = 2;
-  }
-  vector<rc> whites = vector<rc>(W);
-  for (int i = 0; i < W; i++) {
-    ushort r, c;
-    in >> r >> c;
-    whites[i] = rc(r, c);
-    matrix[r][c].type = 1;
-  }
-  // Checking if a rectangle could be the optimal solution
-  if (whites.size() <= 8 && blacks.size() <= 4) {
-    checkForRectangles(N, M, whites, blacks, matrix);
-  }
-  ofstream out("output.txt");
+int main(int argc, char **argv){
 
-  for (int i = 0; i < M; i++) {
-    for (int j = 0; j < N; j++) {
-      cout << matrix[i][j].type << " ";
-    }
-    cout << endl;
-  }
-  return 0;
+	ushort N, M;
+	int B, W;
+	ifstream in("input.txt");
+	// Initialization
+	in >> N >> M >> B >> W;
+	vector<vector<cell>> matrix(M, vector<cell>(N, cell()));
+
+
+	// Forbidding invalid moves
+	for (int i = 0; i < N; i++){
+		matrix[0][i].U = false;
+	}
+	for (int i = 0; i < M; i++){
+		matrix[i][0].L = false;
+		matrix[i][N - 1].R = false;
+	}
+	for (int i = 0; i < N; i++){
+		matrix[M - 1][i].D = false;
+	}
+
+
+	// Getting rings' positions
+	vector<rc> blacks = vector<rc>(B);
+	for (int i = 0; i < B; i++){
+		ushort r, c;
+		in >> r >> c;
+		blacks[i] = rc(r, c);
+		matrix[r][c].type = 2;
+	}
+	vector<rc> whites = vector<rc>(W);
+	for (int i = 0; i < W; i++){
+		ushort r, c;
+		in >> r >> c;
+		whites[i] = rc(r, c);
+		matrix[r][c].type = 1;
+	}
+
+	rc start;
+	if(B>0){
+		start=blacks[0];
+	}else{
+		start=whites[0];
+	}
+	int rings=1;
+	
+
+	ofstream out("output.txt");
+
+	for (int i = 0; i < M; i++){
+		for (int j = 0; j < N; j++){
+			cout << matrix[i][j].type << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+int find(rc start,rc position,vector<vector<cell>> matrix,stringbuf *giro,int rings){
+	if(position==start && rings>1){
+		return rings;
+	}
+	if(position!=start){
+		if(matrix[position.first][position.second].type==1 || matrix[position.first][position.second].type==2){
+			rings++;
+		}
+	}
+	if(matrix[position.first][position.second].R && matrix[position.first+1][position.second].type!=3){
+		giro->sputc('R');
+		matrix[position.first+1][position.second].type=3;
+		rc tmp;
+		tmp.first=position.first+1;
+		tmp.second=position.second;
+		find(start,tmp,matrix,giro);
+	}
+
+
+	if(matrix[position.first][position.second].D && matrix[position.first][position.second+1].type!=3){
+		giro->sputc('D');
+		matrix[position.first][position.second+1].type=3;
+		rc tmp;
+		tmp.first=position.first;
+		tmp.second=position.second+1;
+		find(start,tmp,matrix,giro);
+	}
+	if(matrix[position.first][position.second].L && matrix[position.first-1][position.second].type!=3){
+		giro->sputc('L');
+		matrix[position.first-1][position.second].type=3;
+		rc tmp;
+		tmp.first=position.first-1;
+		tmp.second=position.second;
+		find(start,tmp,matrix,giro);
+	}
+	if(matrix[position.first][position.second].U && matrix[position.first][position.second-1].type!=3){
+		giro->sputc('U');
+		matrix[position.first][position.second-1].type=3;
+		rc tmp;
+		tmp.first=position.first;
+		tmp.second=position.second-1;
+		find(start,tmp,matrix,giro);
+	}
 }
