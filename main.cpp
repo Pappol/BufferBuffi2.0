8a2216a2c3500e212d2e6ae8571c63e0f1615b54
Giacomo Zanolli <giacozanolli+gitlab@gmail.com>
2020-05-24 22:36:58 +0200

    Buonanotte ai sognatori 🌙
    Co-authored-by: RiccardoPeron <riccardo.peron@studenti.unitn.it>
    Co-authored-by: Riccardo Parola <47860088+Pappol@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ typedef struct {
   bool L = true;  // Can go left from here
   bool R = true;  // Can go right from here
   short type = 0; // 0=vuota,1=bianca, 2=nera
+  bool vis = false;
 } cell;
 
 // Globals
@@ -31,6 +32,7 @@ stack<bitset<24>> states;
 // gets printed anyway. Returns ring count w/ a rectangular solution.
 int checkForRectangles();
 void goRadar();
+void placeWalls();
 
 int main() {
   int B, W;
@@ -66,6 +68,7 @@ int main() {
     if (checkForRectangles() == B + W)
       return 0;
   }
+  placeWalls();
   // Go
   goRadar();
 
@@ -134,6 +137,164 @@ int checkForRectangles() {
   return anelli;
 }
 
+void placeWalls() {
+  bool addedSomeWalls;
+  int border = 0;
+  do {
+    addedSomeWalls = false;
+    int n1 = n - border;
+    int m1 = m - border;
+    for (int j = border; j < m1; j++) { // BLUE  TOP
+      cell c = matrix[border][j];
+      if (c.type == BIANCO && c.U == false) {
+        matrix[border][j].D = false;
+        matrix[border + 1][j].U = false;
+        addedSomeWalls = true;
+      }
+    }
+    for (int i = border; i < n1; i++) { // BLUE  RIGHT
+      cell c = matrix[i][m1 - 1];
+      if (c.type == BIANCO && c.R == false) {
+        matrix[i][m1 - 1].L = false;
+        matrix[i][m1 - 2].R = false;
+        addedSomeWalls = true;
+      }
+    }
+    for (int j = border; j < m1; j++) { // BLUE  BOTTOM
+      cell c = matrix[n1 - 1][j];
+      if (c.type == BIANCO && c.D == false) {
+        matrix[n1 - 1][j].U = false;
+        matrix[n1 - 2][j].D = false;
+        addedSomeWalls = true;
+      }
+    }
+    for (int i = border; i < n1; i++) { // BLUE LEFT
+      cell c = matrix[i][border];
+      if (c.type == BIANCO && c.L == false) {
+        matrix[i][border].R = false;
+        matrix[i][border + 1].L = false;
+        addedSomeWalls = true;
+      }
+    }
+    border++;
+  } while (addedSomeWalls);
+
+  for (int i = 1; i < n; i++) // GREEN VERTICAL
+    for (int j = 0; j < m; j++)
+      if (matrix[i][j].type == BIANCO && matrix[i - 1][j].type == BIANCO &&
+          matrix[i - 1][j].D == true) {
+        // Laterali (verticali)
+        matrix[i][j].L = matrix[i][j].R = false;
+        matrix[i - 1][j].L = matrix[i - 1][j].R = false;
+        if (j + 1 < m)
+          matrix[i - 1][j + 1].L = matrix[i][j + 1].L = false;
+        if (j - 1 > 0)
+          matrix[i - 1][j - 1].R = matrix[i][j - 1].R = false;
+        if (i - 2 >= 0) {
+          matrix[i - 2][j].U = false;
+          if (i - 3 >= 0)
+            matrix[i - 3][j].D = false;
+        }
+        if (i + 1 < n) {
+          matrix[i + 1][j].D = false;
+          if (i + 2 < n)
+            matrix[i + 2][j].U = false;
+        }
+      }
+  for (int j = 1; j < m; j++) // GREEN HORIZONTAL
+    for (int i = 0; i < n; i++)
+      if (matrix[i][j].type == BIANCO && matrix[i][j - 1].type == BIANCO &&
+          matrix[i][j - 1].R == true) {
+        // Laterali (orizzontali)
+        matrix[i][j].U = matrix[i][j].D = false;
+        matrix[i][j - 1].U = matrix[i][j - 1].D = false;
+        if (i - 1 >= 0)
+          matrix[i - 1][j].D = matrix[i - 1][j - 1].D = false;
+        if (i + 1 < n)
+          matrix[i + 1][j].U = matrix[i + 1][j - 1].U = false;
+        if (j - 2 >= 0) {
+          matrix[i][j - 2].L = false;
+          if (j - 3 >= 0)
+            matrix[i][j - 3].R = false;
+        }
+        if (j + 1 < m) {
+          matrix[i][j + 1].R = false;
+          if (j + 2 < m)
+            matrix[i][j + 2].L = false;
+        }
+      }
+  // TODO: rosa
+}
+
 void goRadar() {
-  
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      matrix[i][j].vis = false;
+  rc cur = blacks[0];
+  vector<bool> visited = vector<bool>(blacks.size());
+}
+
+bool controlDiag(char dir, rc cella) {
+  switch (dir) {
+  case 'A':
+    return cella.first > 0 && cella.second + 1 < m;
+  case 'B':
+    return cella.first + 1 < n && cella.second + 1 < m;
+  case 'C':
+    return cella.first + 1 < n && cella.second > 0;
+  case 'D':
+    return cella.first > 0 && cella.second > 0;
+  }
+}
+
+rc findNear(rc pos) {
+  int giro = 0;
+  int misuraLato = 2;
+  rc u1, u2, l1, l2, r1, r2, d1, d2;
+  u1 = u2 = l1 = l2 = r1 = r2 = d1 = d2 = pos;
+  for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < misuraLato; i++) {
+      switch (j) {
+      case 0: // UP
+        if (controlDiag('A', u2) && controlDiag('D', u1)) {
+          u1.first--;
+          u1.second--;
+          u2.first--;
+          if (giro != 0)
+            u2.second++;
+        }
+        break;
+      case 1: // LEFT
+        if (controlDiag('A', r1) && controlDiag('B', r2)) {
+          r1.first--;
+          r1.second++;
+          r2.first++;
+          if (giro != 0)
+            r2.second++;
+        }
+        break;
+      case 2: // DOWN
+        if (controlDiag('B', d1) && controlDiag('C', d2)) {
+          d1.first++;
+          d1.second++;
+          d2.first++;
+          if (giro != 0)
+            d2.second--;
+        }
+        break;
+      case 3: // RIGHT
+        if (controlDiag('C', l1) && controlDiag('D', l2)) {
+          l1.first++;
+          l1.second--;
+          l2.first--;
+          if (giro != 0)
+            l2.second--;
+        }
+        break;
+      }
+    }
+  }
+}
+
+string makePath(rc from, rc to){
 }
